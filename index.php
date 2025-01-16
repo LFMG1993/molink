de4c725759a9d5636cb8a49f835474e85174e179
LFMG1993 <lfmontes662@soy.sena.edu.co>
2025-01-15 21:55:39 -0500

    ajustes al login en proceso

--- index.php
+++ index.php
@@ -57,8 +57,8 @@
 
             <div class="row justify-content-center text-center" data-aos="fade-up" data-aos-delay="100">
                 <div class="col-xl-6 col-lg-8">
-                    <h2>Powerful Digital Solutions With GP<span>.</span></h2>
-                    <p>We are team of talented digital marketers</p>
+                    <h2>Ofrecemos Multiples Soluciones Tecnologicas</h2>
+                    <p>Abriendo nuevos horizontes</p>
                 </div>
             </div>
 
@@ -66,31 +66,31 @@
                 <div class="col-xl-2 col-md-4" data-aos="fade-up" data-aos-delay="300">
                     <div class="icon-box">
                         <i class="bi bi-binoculars"></i>
-                        <h3><a href="">Lorem Ipsum</a></h3>
+                        <h3><a href="">Desarrollo Web</a></h3>
                     </div>
                 </div>
                 <div class="col-xl-2 col-md-4" data-aos="fade-up" data-aos-delay="400">
                     <div class="icon-box">
                         <i class="bi bi-bullseye"></i>
-                        <h3><a href="">Dolor Sitema</a></h3>
+                        <h3><a href="">Desarrollo Movil</a></h3>
                     </div>
                 </div>
                 <div class="col-xl-2 col-md-4" data-aos="fade-up" data-aos-delay="500">
                     <div class="icon-box">
                         <i class="bi bi-fullscreen-exit"></i>
-                        <h3><a href="">Sedare Perspiciatis</a></h3>
+                        <h3><a href="">Diseño UI/UX</a></h3>
                     </div>
                 </div>
                 <div class="col-xl-2 col-md-4" data-aos="fade-up" data-aos-delay="600">
                     <div class="icon-box">
                         <i class="bi bi-card-list"></i>
-                        <h3><a href="">Magni Dolores</a></h3>
+                        <h3><a href="">Hardware</a></h3>
                     </div>
                 </div>
                 <div class="col-xl-2 col-md-4" data-aos="fade-up" data-aos-delay="700">
                     <div class="icon-box">
                         <i class="bi bi-gem"></i>
-                        <h3><a href="">Nemos Enimade</a></h3>
+                        <h3><a href="">Software</a></h3>
                     </div>
                 </div>
             </div>
